9a77d3c37b1f6f822d0e0892135e629ff648a817
Odnt <remyalexd2003@gmail.com>
2023-10-22 18:59:11 -0500

    Added Check For Winner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const gameboard = document.querySelector("#board");
     const startCells = ["", "", "", "", "", "", "", "", ""];
     let player = 'X';
+    const display = document.querySelector('#status');
+    const newGameButton = document.querySelector('.btn');
+    let boardState = Array.from({ length: startCells.length });
 
     function createBoard() {
         startCells.forEach((cell, index) => {
@@ -17,9 +20,9 @@ document.addEventListener('DOMContentLoaded', function () {
    const squares = document.querySelectorAll('.square');
    var check = 0
 
-    squares.forEach(square => {
+    squares.forEach((square, index) => {
         square.addEventListener('mouseover', function () {
-            this.classList.add('hover');
+            this.classList.add('hover'); 
         });
 
         square.addEventListener('mouseout', function () {
@@ -28,21 +31,83 @@ document.addEventListener('DOMContentLoaded', function () {
 
         square.addEventListener('click', function () {
             if (!this.textContent)  {
-                this.classList.add(player) 
-                this.textContent = player
+                this.classList.add(player);
+                this.textContent = player;
+                boardState[index] = player;
+                const winner = checkWinner(player);
+                console.log(winner)
+                if (winner) {
+                    console.log(winner)
+                    console.log(`Congratulations!${player} wins!`);
+                } else {
+                    console.log("No winner yet.");
+                    }
                 
 
                 if (player == 'X'){
                     player = 'O';
-
                 }
                 else
                 {
                     player = 'X'
                 }
+                
             }
+           
         });
     });
+    function checkWinner(currentClass) {
+      
+        const WINNING_COMBINATIONS = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+    
+        for (const i of WINNING_COMBINATIONS) {
+            const [a, b, c] = i;
+    
+            console.log(`Checking combination: ${boardState[a]}, ${boardState[b]}, ${boardState[c]}`);
+    
+            if (
+                boardState[a] === currentClass &&
+                boardState[b] === currentClass &&
+                boardState[c] === currentClass
+            ) {
+                console.log(`Winner found at combination: ${boardState[a]}, ${boardState[b]}, ${boardState[c]}`);
+                return true; 
+            }
+        }
+    
+       
+    
+        return false;
+    }
+    
+
+    
+   
+    
+
+    newGameButton.addEventListener('click', function(){
+        resetGame();
+    })
+    
+    
+
+    function resetGame() {
+        squares.forEach(square => {
+            square.textContent = '';
+            square.classList.remove('X', 'O'); 
+        });
+        player = 'X'
+        display.textContent = "Move your mouse over a square and click to play an X or an O."
+    }
 
 
 });
